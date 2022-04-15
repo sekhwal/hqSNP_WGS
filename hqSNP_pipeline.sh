e0053c69812cb69abbf96b822f207e8ab4aca7db
Kumar <sekhwal@gmail.com>
2022-04-15 15:19:28 -0400

    Update hqSNP_pipeline.sh

--- hqSNP_pipeline.sh
+++ hqSNP_pipeline.sh
@@ -10,7 +10,7 @@ do bbduk.sh -Xmx1g in=$i in2=${i/_R1/_R2} out=${i/_R1/_trim_R1} out2=${i/_R1/_tr
 done
 
 #########Calculating stat
-#cd ./bbduk_results/
+
 seqkit stat *.fastq.gz > stat_run.txt
 
 #converting space to tab tab-delimited
@@ -18,15 +18,9 @@ sed 's/ \+/\t/g' stat_run.txt > stat_.txt
 #Removing files
 rm *stat_run*
 
-#cp runme.sh ./bbduk_results
-#cp s.dublin-full.gb ./bbduk_results
-
-#curr=/media/mmk6053/Data/Manoj_data/Dublin_project/snippy_try/bbduk_results
-#cd $curr
-
 #Running snippy
 echo "snippy running"
-#snippy-multi input.tab --ref *.gb --cpus 40 > runme.sh
+snippy-multi input.tab --ref *.gb --cpus 40 > runme.sh
 sh ./runme.sh
 
 echo "remove all the weird characters and replace them with N"
